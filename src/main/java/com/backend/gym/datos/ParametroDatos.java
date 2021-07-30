b56fd970c5b345c905fde2660cdda261f936eaff
Jorge Alejandro <alejoved@gmail.com>
2021-07-29 22:43:07 -0500

    mod

--- src/main/java/com/backend/gym/datos/ParametroDatos.java
+++ src/main/java/com/backend/gym/datos/ParametroDatos.java
@@ -30,19 +30,19 @@ public class ParametroDatos implements ApplicationRunner {
             parametros.add(new Parametro("MINUTOS", "MIN", "MEDIDATIEMPO", true));
             parametros.add(new Parametro("HORAS", "HORAS", "MEDIDATIEMPO", true));
             //SLIDERS
-            parametros.add(new Parametro("", "slider1.jpeg", "SLIDER", false));
-            parametros.add(new Parametro("", "slider2.jpeg", "SLIDER", false));
-            parametros.add(new Parametro("", "slider3.jpeg", "SLIDER", false));
-            parametros.add(new Parametro("", "slider4.jpeg", "SLIDER", true));
-            parametros.add(new Parametro("", "slider5.jpeg", "SLIDER", true));
-            parametros.add(new Parametro("", "slider6.jpeg", "SLIDER", true));
-            parametros.add(new Parametro("", "slider7.jpeg", "SLIDER", true));
-            parametros.add(new Parametro("", "slider8.jpeg", "SLIDER", true));
-            parametros.add(new Parametro("", "slider9.jpeg", "SLIDER", true));
-            parametros.add(new Parametro("", "slider10.jpeg", "SLIDER", true));
-            parametros.add(new Parametro("", "slider11.jpeg", "SLIDER", true));
-            parametros.add(new Parametro("", "slider12.jpeg", "SLIDER", true));
-            parametros.add(new Parametro("", "slider13.jpeg", "SLIDER", true));
+            parametros.add(new Parametro("", "slider1.jpg", "SLIDER", false));
+            parametros.add(new Parametro("", "slider2.jpg", "SLIDER", false));
+            parametros.add(new Parametro("", "slider3.jpg", "SLIDER", false));
+            parametros.add(new Parametro("", "slider4.jpg", "SLIDER", true));
+            parametros.add(new Parametro("", "slider5.jpg", "SLIDER", true));
+            parametros.add(new Parametro("", "slider6.jpg", "SLIDER", true));
+            parametros.add(new Parametro("", "slider7.jpg", "SLIDER", true));
+            parametros.add(new Parametro("", "slider8.jpg", "SLIDER", true));
+            parametros.add(new Parametro("", "slider9.jpg", "SLIDER", true));
+            parametros.add(new Parametro("", "slider10.jpg", "SLIDER", true));
+            parametros.add(new Parametro("", "slider11.jpg", "SLIDER", true));
+            parametros.add(new Parametro("", "slider12.jpg", "SLIDER", true));
+            parametros.add(new Parametro("", "slider13.jpg", "SLIDER", true));
             //SERVICIOS
             parametros.add(new Parametro("", "servicio1.jpeg", "SERVICIO", true));
             parametros.add(new Parametro("", "servicio2.jpeg", "SERVICIO", true));
